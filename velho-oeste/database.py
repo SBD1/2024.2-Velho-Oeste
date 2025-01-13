ff81aad2c8c7ef4f4a077973135ba7be74529ed7
Daniel Sousa <sousadanielsb@gamil.com>
2025-01-13 20:09:47 -0300

    Fix: Correção de Conexão

--- velho-oeste/database.py
+++ velho-oeste/database.py
@@ -3,7 +3,7 @@ import psycopg2
 class DataBase ():
     def create_connection(self):
         connect = psycopg2.connect(
-            host="localhost",
+            host="postgres",
             database="velho_oeste_db",
             user="velho_oeste_user",
             password="vopassword")
